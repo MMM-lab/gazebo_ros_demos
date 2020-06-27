1a9714db95ee051cec763463cfa4074975a00a53
RyosukeMatsushima <m.ryosuke@me.com>
2020-06-27 16:46:59 +0900

    add actuator_interface

---
+++ rrbot_control/src/actuator_interface.cpp
@@ -0,0 +1,38 @@
+#include "ros/ros.h"
+#include "std_msgs/Float64.h"
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "talker");
+
+  ros::NodeHandle n;
+
+  ros::Publisher joint1_pub = n.advertise<std_msgs::Float64>("/rrbot/joint1_torque/command", 1000);
+  ros::Publisher joint2_pub = n.advertise<std_msgs::Float64>("/rrbot/joint2_torque/command", 1000);
+
+  ros::Rate loop_rate(10);
+
+  int count = 0;
+  while (ros::ok())
+  {
+    std_msgs::Float64 joint1_torque;
+    std_msgs::Float64 joint2_torque;
+
+    joint1_torque.data = -0;
+    joint2_torque.data = -2;
+
+    ROS_INFO("%f", joint1_torque.data);
+    ROS_INFO("%f", joint2_torque.data);
+
+    joint1_pub.publish(joint1_torque);
+    joint2_pub.publish(joint2_torque);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
+
+  return 0;
+}
