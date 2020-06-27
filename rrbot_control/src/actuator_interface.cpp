9b82484e25f3a33e01323b77d77dd9c05cee406c
RyosukeMatsushima <m.ryosuke@me.com>
2020-06-27 23:19:27 +0900

    add joint state subscriber

--- rrbot_control/src/actuator_interface.cpp
+++ rrbot_control/src/actuator_interface.cpp
@@ -1,16 +1,25 @@
 #include "ros/ros.h"
 #include "std_msgs/Float64.h"
+#include "sensor_msgs/JointState.h"
 
 #include <string>
 
+void subscribeState(const sensor_msgs::JointState rrobotState)
+{
+  ROS_INFO("joint1 position %f", rrobotState.position[0]);
+  ROS_INFO("joint2 position %f", rrobotState.position[1]);
+}
+
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "actator_interface");
 
   ros::NodeHandle n;
 
   ros::Publisher joint1_pub = n.advertise<std_msgs::Float64>("/rrbot/joint1_torque/command", 1000);
   ros::Publisher joint2_pub = n.advertise<std_msgs::Float64>("/rrbot/joint2_torque/command", 1000);
+
+  ros::Subscriber sub = n.subscribe("/rrbot/joint_states", 1000, subscribeState);
 
   ros::Rate loop_rate(10);
 
@@ -23,8 +32,8 @@ int main(int argc, char **argv)
     joint1_torque.data = std::stof(argv[1]);
     joint2_torque.data = std::stof(argv[2]);
 
-    ROS_INFO("%f", joint1_torque.data);
-    ROS_INFO("%f", joint2_torque.data);
+    // ROS_INFO("%f", joint1_torque.data);
+    // ROS_INFO("%f", joint2_torque.data);
 
     joint1_pub.publish(joint1_torque);
     joint2_pub.publish(joint2_torque);
